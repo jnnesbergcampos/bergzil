264e16769b811f788d2c5302939ff5c60d6dc36f
Joannesberg C. da Costa <142360802+jnnesbergcampos@users.noreply.github.com>
2026-09-21 00:07:03 +0000

    init2

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 def main():
 	nome = input("Qual é o seu nome? ").strip()
-    idade = int(input("Qual é a sua idade? "))
+
+	try:
+		idade = int(input("Qual é a sua idade? "))
+	except ValueError:
+		print("Digite uma idade válida usando apenas números.")
+		return
 
 	if nome:
-		print(f"Olá, {nome}! A espanha te espera")
-    if idade > 18:
-        print("Você é maior de idade.")
-    if idade < 18:
-        print("Você é menor de idade.")
+		print(f"Olá, {nome}! A Espanha te espera!")
 	else:
-		print("Olá! a espanha te espera")
+		print("Olá! A Espanha te espera!")
 
+	if idade >= 18:
+		print("Você é maior de idade.")
+	else:
+		print("Você é menor de idade.")
 
 if __name__ == "__main__":
-	main()
+    main()
